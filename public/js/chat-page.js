13233b7e22840e38cc1559b235e2fc02243dfec1
Baba0608 <babafakruddinnitturu@gmail.com>
2024-01-01 20:00:28 +0530

    chat page frontend somewhat similar to what's app

--- public/js/chat-page.js
+++ public/js/chat-page.js
@@ -2,29 +2,101 @@ const website = "http://localhost:4000";
 const sendButton = document.getElementById("send-button");
 const messagesDiv = document.getElementById("messages-container");
 
+const container = document.getElementById("container");
+const chatContainer = document.getElementById("chat-container");
+const messagesContainer = document.getElementById("messages-container");
+
+const chats = document.querySelectorAll(".chat");
+
+const backArrow = document.getElementById("back-image");
+let ACTIVE_CHAT = false;
+
 const token = localStorage.getItem("chat-app-token");
 
 (async function () {
-  const joinedMessage = createMessage("You Joined");
-  messagesDiv.appendChild(joinedMessage);
+  // const joinedMessage = createMessage("You Joined");
+  // messagesDiv.appendChild(joinedMessage);
+  // const result = await axios.get(`${website}/chat/getmessages`, {
+  //   headers: { authorization: token },
+  // });
+  // const userId = result.data.userId;
+  // result.data.result.forEach((message) => {
+  //   if (message.id === userId) {
+  //     const msg = createMessage(`You: ${message.message}`);
+  //     messagesDiv.appendChild(msg);
+  //   } else {
+  //     const msg = createMessage(`${message.user.username}: ${message.message}`);
+  //     messagesDiv.appendChild(msg);
+  //   }
+  // });
+})();
 
-  const result = await axios.get(`${website}/chat/getmessages`, {
-    headers: { authorization: token },
-  });
+// media queries
+const x = window.matchMedia("(min-width: 750px)");
 
-  //   console.log(result);
+function greaterThan750(x) {
+  if (x.matches) {
+    messagesContainer.style.display = "block";
+    chatContainer.style.display = "block";
+  } else {
+  }
+}
 
-  const userId = result.data.userId;
-  result.data.result.forEach((message) => {
-    if (message.id === userId) {
-      const msg = createMessage(`You: ${message.message}`);
-      messagesDiv.appendChild(msg);
+x.addEventListener("change", (e) => {
+  e.preventDefault();
+  greaterThan750(x);
+});
+
+const y = window.matchMedia("(max-width: 750px)");
+
+function lessThan750(y) {
+  if (y.matches) {
+    if (ACTIVE_CHAT) {
+      messagesContainer.style.display = "block";
+      chatContainer.style.display = "none";
     } else {
-      const msg = createMessage(`${message.user.username}: ${message.message}`);
-      messagesDiv.appendChild(msg);
+      chatContainer.style.display = "flex";
+      messagesContainer.style.display = "none";
+    }
+  }
+}
+
+y.addEventListener("change", (e) => {
+  e.preventDefault();
+  lessThan750(y);
+});
+
+// ----------------------------------------------------
+
+chats.forEach((chat) => {
+  chat.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    ACTIVE_CHAT = true;
+    chats.forEach((chat) => {
+      chat.classList.remove("active");
+    });
+
+    chat.classList.add("active");
+
+    const width = container.offsetWidth;
+    if (width < 750) {
+      chatContainer.style.display = "none";
+      messagesContainer.style.display = "block";
     }
   });
-})();
+});
+
+backArrow.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  chats.forEach((chat) => {
+    chat.classList.remove("active");
+  });
+  ACTIVE_CHAT = false;
+  messagesContainer.style.display = "none";
+  chatContainer.style.display = "flex";
+});
 
 function createMessage(msg) {
   const message = document.createElement("div");
